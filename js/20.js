2ac07a3fcd8e203b09868bdd4394ef792687b4d9
Hamilton Durán <hamy.duran@gmail.com>
2023-11-13 00:06:46 -0600

    Operador ternario

--- js/20.js
+++ js/20.js
@@ -1,12 +1,18 @@
 //Operadores Ternearios
 const autenticado = true
 autenticado ? console.log("usuario autenticado") : console.log("No autenticado")
-
+// Lo que esta dentro de    ?   y   : es lo  que se ejecuta si la condicion se cumple
+// de los : a la derecha es lo que se ejecutara lo que no se cumple
+//y del ? a la izquierda es la condicion
 //Doble ternario
 
 const saldo = 600
 const pagar = 700
 const tarjeta =  true
 
-saldo > pagar ? console.log("Puedes pagar") : tarjeta ? console.log("Puedes pagar Tarjeta") : console.log("no puedes pagar ")
+saldo > pagar ?
+    console.log("Puedes pagar") :
+    tarjeta ? //Segunda condicion
+    console.log("Puedes pagar Tarjeta") 
+    : console.log("no puedes pagar ")
 
