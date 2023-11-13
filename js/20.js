6e8dbfeba323c12882f0db4790636b1a633828af
Hamilton Durán <hamy.duran@gmail.com>
2023-11-13 00:00:26 -0600

    Operador ternario y condicionakles

---
+++ js/20.js
@@ -0,0 +1,12 @@
+//Operadores Ternearios
+const autenticado = true
+autenticado ? console.log("usuario autenticado") : console.log("No autenticado")
+
+//Doble ternario
+
+const saldo = 600
+const pagar = 700
+const tarjeta =  true
+
+saldo > pagar ? console.log("Puedes pagar") : tarjeta ? console.log("Puedes pagar Tarjeta") : console.log("no puedes pagar ")
+
